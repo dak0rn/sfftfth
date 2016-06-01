9df1dfc8f0b3379acde7eadb7e547aa609e752b4
Daniel Koch <daniel@suitsoft.eu>
2016-06-01 06:21:25 +0200

    Tests for the file writer

---
+++ test/write-files.test.js
@@ -0,0 +1,85 @@
+/**
+ * Tests for the file writer
+ */
+
+const test = require('tape');
+const utils = require('./fixture');
+const prunk = require('prunk');
+const Immutable = require('immutable');
+
+const setup = (mkdirpMock, fsMock) => {
+    prunk.mock('mkdirp', mkdirpMock);
+    prunk.mock('fs-promise', fsMock);
+
+    return require('../lib/write-files');
+};
+
+// Teardown utility
+const teardown = () => utils.resetTestDoubles('../lib/write-files');
+
+// Default mocks
+const defFs = {
+    writeFile() {
+        return Promise.resolve();
+    }
+};
+
+const defConfig = Immutable.Map();
+
+const defMkdir = (_, cb) => cb(null);
+
+test('write-files; exports a function', t => {
+
+    const writeFiles = setup();
+    t.plan(1);
+    t.equals(typeof writeFiles, 'function');
+    t.end();
+    teardown();
+});
+
+test('write-files; creates the output directories of files', t => {
+    const files = Immutable.fromJS([
+        { outputDirectory: 'a' },
+        { outputDirectory: 'b' },
+        { outputDirectory: 'c' }
+    ]);
+    const dirs = files.map( f => f.get('outputDirectory') );
+
+    const mock = (name, cb) => {
+        t.true( dirs.contains(name) );
+        cb(null);
+    };
+
+    t.plan( files.count() );
+
+    const writeFiles = setup(mock, defFs);
+
+    writeFiles(defConfig, files)
+        .then( () => t.end() )
+        .catch( () => t.fail() );
+});
+
+test('write-files; writes the files', t => {
+    const files = Immutable.fromJS([
+        { outputPath: 'a', contents: 'a' },
+        { outputPath: 'b', contents: 'b' },
+        { outputPath: 'c', contents: 'c' }
+    ]);
+    const paths = files.map( f => f.get('outputDirectory') );
+
+    const mock = {
+        writeFile(file, contents) {
+            t.equal(file, contents);
+            t.true( paths.contains(file) );
+            return Promise.resovlve();
+        }
+    };
+
+    t.plan( files.count() );
+
+    const writeFiles = setup(defMkdir, mock);
+
+    writeFiles(defConfig, files)
+        .then( () => t.end() )
+        .catch( () => t.fail() );
+});
